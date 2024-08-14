0860f8b1d3f7f2f5448ca18088703b426a59c3ad
Bertrand Caron <bertrand.fx.caron@gmail.com>
2024-08-14 10:27:33 +1000

    Feat: Add prettier plugin to sort JSON objects

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,8 @@
 const config = {
+  plugins: ["prettier-plugin-sort-json"],
   semi: false, // Do not add redundant characters
   trailingComma: "all", // Smaller git diffs
+  jsonRecursiveSort: true, // More canonical JSON objects (requires `prettier-plugin-sort-json` plugin)
 }
 
 export default config
